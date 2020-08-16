9bd9508db1586297ddf827112c6bd2c3b7289af6
MrBlueXav <bluexav@free.fr>
2020-08-16 11:25:17 +0200

    freeze function + clean up

--- pod/DekrispatorDroneBox/Dekrispator.cpp
+++ pod/DekrispatorDroneBox/Dekrispator.cpp
@@ -35,14 +35,10 @@ using namespace daisy;
 
 static DaisyPod pod;
 
-static Parameter pitchParam, cutoffParam, lfoParam;
 bool freeze = false;
 bool demoMode = true;
 
-int wave, mode;
-
 float oldk1, oldk2, k1, k2;
-bool selfCycle;
 
 void ConditionalParameter(float oldVal, float newVal, float &param, float update);
 
@@ -51,9 +47,8 @@ void Controls();
 static void AudioCallback(float *in, float *out, size_t size)
 {
     Controls();
-    //float	 	y = 0;
+  
 	float	 	yL, yR ;
-	//float 		f1;
     
     for (size_t i = 0; i < size; i += 2)
     {
@@ -71,25 +66,18 @@ int main(void)
 {
     // Set global variables
     float sample_rate;
-    mode = 0;
-   
+      
     oldk1 = oldk2 = 0;
     k1 = k2 = 0;
    
-    selfCycle = false;
-    
     //Init everything
     pod.Init();
     sample_rate = pod.AudioSampleRate();
 
-    randomGen_init();/* Initialize the on-board random number generator ! */
+    /* Initialize the on-board random number generator ! */
+    randomGen_init();
     Synth_Init();
 
-       //set parameter parameters
-   /*  cutoffParam.Init(pod.knob1, 100, 20000, cutoffParam.LOGARITHMIC);
-    pitchParam.Init(pod.knob2, 50, 5000, pitchParam.LOGARITHMIC);
-    lfoParam.Init(pod.knob1, 0.25, 1000, lfoParam.LOGARITHMIC); */
-    
     // start callback
     pod.StartAdc();
     pod.StartAudio(AudioCallback);
@@ -110,33 +98,17 @@ void ConditionalParameter(float oldVal, float newVal, float &param, float update
 //Controls Helpers
 void UpdateEncoder()
 {
-    wave += pod.encoder.RisingEdge();
-   
-
-    //skip ramp since it sounds like saw
-    if (wave == 3)
-    {  
-        wave = 4;
-    }
-
-   
-    
-    mode += pod.encoder.Increment();
-    mode = (mode % 3 + 3) % 3;
+  
 }
 
 void UpdateKnobs()
 {
     k1 = pod.knob1.Process();
-    k2 = pod.knob2.Process();
-
-  
+    k2 = pod.knob2.Process();  
 }
 
 void UpdateLeds()
 {
-    pod.led1.Set(mode == 2, mode == 1, mode == 0);
-    pod.led2.Set(0, selfCycle, selfCycle);
     
     oldk1 = k1;
     oldk2 = k2;
@@ -146,14 +118,17 @@ void UpdateLeds()
 
 void UpdateButtons()
 {
-    if (pod.button2.RisingEdge() )
-    {
-  
-    }
-
     if (pod.button1.RisingEdge())
     {
-        freeze = !freeze;
+       
+    }
+
+     if (pod.button2.RisingEdge() )
+    {
+         freeze = !freeze;
+        if (freeze)  pod.led1.Set(1.0f, 1.0f, 1.0f);
+        else pod.led1.Set(0, 0, 0);
+  
     }
 }
 
@@ -166,7 +141,7 @@ void Controls()
 
     UpdateKnobs();
 
-    UpdateLeds();
-
     UpdateButtons();
+
+    UpdateLeds();    
 }
